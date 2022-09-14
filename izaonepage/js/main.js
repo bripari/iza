0519ee77dc8bf5860fca8ee69ff9c2a9918b9f4b
bripari <108435005+bripari@users.noreply.github.com>
2022-09-14 17:18:02 -0400

    single page demo

---
+++ izaonepage/js/main.js
@@ -0,0 +1,38 @@
+//ANIMATE ON SCROLL
+
+const observer = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('slide-in-tr');
+    }
+  });
+});
+
+observer.observe(document.querySelector('.animate'));
+
+const observer2 = new IntersectionObserver(entries => {
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('fade-in');
+    }
+  });
+});
+
+observer2.observe(document.querySelector('.animate2'));
+observer2.observe(document.querySelector('.animate3'));
+
+
+
+
+// Scroll down button
+const scrollButton = () => {
+  document.getElementById("polterDetails").scrollIntoView({behavior: "smooth"})
+ }
+document.getElementById("scrollBtn").addEventListener("click", scrollButton)
+
